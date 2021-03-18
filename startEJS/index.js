7119782d7b48dcbb45c74d9c0cb3b10aed37d26b
lizcramerfox <ecfox2008@gmail.com>
2021-03-18 17:00:26 -0400

    Adds form/message functionality

--- startEJS/index.js
+++ startEJS/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-var path = require('path');
-const app = express();
-const port = 3000;
+let path = require('path')
+const app = express()
+const port = 3000
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // setup public folder for static files
 app.use(express.static('./public'))
@@ -28,6 +28,35 @@ app.get('/links', (req, res) => {
 app.get('/list', (req, res) => {
   let items = ['node.js', 'expressjs', 'ejs', 'javascript', 'bootstrap']
   res.render('pages/list', { list: items })
+})
+
+app.get('/table', (req, res) => {
+  let items = [
+    {name:'node.js',url:'https://nodejs.org/en/'},
+    {name:'ejs',url:'https://ejs.co'},
+    {name:'expressjs',url:'https://expressjs.com'},
+    {name:'vuejs',url:'https://vuejs.org'},
+    {name:'nextjs',url:'https://nextjs.org'}
+  ]
+  res.render('pages/table', { table: items })
+})
+
+// Middleware for alert messages
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+function messages(req, res, next) {
+  let message
+  res.locals.message = message
+  next()
+}
+app.get('/form', messages, (req, res) => {
+  res.render('pages/form')
+})
+
+app.post('/form', (req, res) => {
+  let message = req.body
+  res.locals.message = message
+  res.render('pages/form') 
 })
 
 app.listen(port, () => console.log(`MasterEJS app Started on port ${port}!`))
